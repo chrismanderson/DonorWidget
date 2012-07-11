2ffc4cf62b9435797bc8e0abacaa137920d75e12
Chris Anderson <christopher.anderson@gmail.com>
2012-07-11 13:39:16 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DonorsWidget::Application.config.secret_token = '4dfeb0b8c393121ac5563d1be48379609720ec1ac5c2e8eb28d22f6b90f3e219ca0d7de8e84a1c3ba50fd442f6d8f182dd1b9b8ae8b73087b1936485756c6fe2'
