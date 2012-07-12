4f020ce9c1ba1278d5b7270efb2ff599e69396d5
Chris Anderson <christopher.anderson@gmail.com>
2012-07-12 14:22:32 -0400

    More specs

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -8,11 +8,18 @@ describe Project do
     end
   end
 
-  context '#create_from_url' do
+  context '.create_from_url' do
     use_vcr_cassette
     it 'creates a project with the right attributes' do
       Project.create_from_url 'http://www.donorschoose.org/project/dna-mysteries/797331/'
       Project.first.pid.should == 797331
+    end
+  end
+
+  context '.build_from_struct' do
+    use_vcr_cassette
+    it "creates a hash from a DonorsChoose struct" do
+      
     end
   end
 end
