13310541bca8795fce3c3d42c51ff20280ea945c
Chris Anderson <christopher.anderson@gmail.com>
2012-07-12 10:10:41 -0400

    Created project schema

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'rails', '3.2.6'
 
 gem 'sqlite3'
 gem 'faraday'
+gem 'donors_choose', path: '../donors_choose'
 
 
 # Gems used only for assets and not required
--- db/migrate/20120711181831_create_projects.rb
+++ db/migrate/20120711181831_create_projects.rb
@@ -1,7 +1,31 @@
 class CreateProjects < ActiveRecord::Migration
   def change
     create_table :projects do |t|
-
+      t.integer   :project_id
+      t.string    :proposal_url
+      t.string    :proposal_raw_url
+      t.string    :fund_url
+      t.string    :image_url
+      t.string    :title
+      t.string    :short_description
+      t.string    :fulfillment_trailer
+      t.integer   :percent_funded
+      t.decimal   :cost_to_complete
+      t.decimal   :total_price
+      t.string    :teacher_name
+      t.string    :grade_level
+      t.string    :poverty_level
+      t.string    :school_name
+      t.string    :city
+      t.integer   :zip
+      t.string    :state_abbr
+      t.decimal   :latitude
+      t.decimal   :longitude
+      t.string    :state
+      t.string    :subject
+      t.string    :resource
+      t.datetime  :expiration_date
+      t.string    :funding_status 
       t.timestamps
     end
   end
