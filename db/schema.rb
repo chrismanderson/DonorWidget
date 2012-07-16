e64d83fc0d1023d5373e10c61ee402ba2cb03ebb
Chris Maddox <tyre77@gmail.com>
2012-07-16 15:37:19 -0400

    render template in widget#show

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,34 @@
 
 ActiveRecord::Schema.define(:version => 20120712143948) do
 
+  create_table "projects", :force => true do |t|
+    t.integer  "pid"
+    t.string   "proposal_url"
+    t.string   "fund_url"
+    t.string   "image_url"
+    t.string   "title"
+    t.string   "short_description"
+    t.string   "fulfillment_trailer"
+    t.integer  "percent_funded"
+    t.decimal  "cost_to_complete"
+    t.decimal  "total_price"
+    t.string   "teacher_name"
+    t.string   "grade_level"
+    t.string   "poverty_level"
+    t.string   "school"
+    t.string   "city"
+    t.integer  "zip"
+    t.string   "state"
+    t.decimal  "latitude"
+    t.decimal  "longitude"
+    t.string   "subject"
+    t.string   "resource"
+    t.datetime "expiration_date"
+    t.string   "funding_status"
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
+  end
+
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
@@ -23,7 +51,7 @@ ActiveRecord::Schema.define(:version => 20120712143948) do
 
   create_table "widgets", :force => true do |t|
     t.integer  "user_id"
-    t.string   "url"
+    t.integer  "project_id"
     t.string   "background_color"
     t.string   "size"
     t.string   "rollover_method"
