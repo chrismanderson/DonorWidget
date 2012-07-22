acebba3c2e2e10fdace89cd8fbfc10600c4eaa7d
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-07-22 14:20:56 -0400

    Update REDIS configuration for heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,10 @@
-REDIS = Redis.new(:host => 'localhost', :port => 6379)
+if Rails.env.production?
+  REDIS_URL = 'chubb.redistogo.com'
+  REDIS_PORT = 9533
+  REDIS_PASSWORD = 'efbdbca0dca7bbc447f2d9dcdfb9c6cf'
+else
+  REDIS_URL = 'localhost'
+  REDIS_PORT = 6379
+  REDIS_PASSWORD = nil
+end
+REDIS = Redis.new(:host => REDIS_URL, :port => REDIS_PORT, :password => REDIS_PASSWORD)
