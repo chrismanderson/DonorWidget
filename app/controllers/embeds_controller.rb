4ab32960844c65b4e7fdcd4a418664bd0d7dce6a
Chris Maddox <tyre77@gmail.com>
2012-07-23 15:35:56 -0400

    COLORS AND WIDGETS AND WIZARDS OH MY¡

--- app/controllers/embeds_controller.rb
+++ app/controllers/embeds_controller.rb
@@ -6,6 +6,6 @@ class EmbedsController < ApplicationController
 
   def to_string
     @widget = Widget.find(cookies[:widget_id])
-    @content = render_to_string(:partial => "embeds/widget", :locals => { :widget => @widget}, :layout => false )
+    @content = render_to_string(partial: "embeds/widget", locals: {widget: @widget}, layout: false )
   end
 end
