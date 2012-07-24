4a25e10a9854f1ba59e4bfcda35ba36d72ebf002
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-07-24 19:41:35 -0400

    Fix reek & cane

--- app/controllers/widget_steps_controller.rb
+++ app/controllers/widget_steps_controller.rb
@@ -1,5 +1,6 @@
 class WidgetStepsController < ApplicationController
   include Wicked::Wizard
+  before_filter :find_or_create_widget_from_params, only: :update
 
   layout "wizard"
 
@@ -13,17 +14,22 @@ class WidgetStepsController < ApplicationController
   end
 
   def update
-    @widget = (step == :project) ?
-      Widget.create(params[:widget]) :
-      Widget.find_by_id(cookies[:widget_id]).tap do |w|
-        w.attributes = params[:widget]
-        w.save
-    end
     cookies[:widget_id] = @widget.id
     render_wizard @widget
   end
 
   def finish_wizard_path
     widget_path(@widget)
+  end
+
+  private
+
+  def find_or_create_widget_from_params
+    @widget = (step == :project) ?
+      Widget.create(params[:widget]) :
+      Widget.find_by_id(cookies[:widget_id]).tap do |widget|
+        widget.attributes = params[:widget]
+        widget.save
+    end
   end
 end
