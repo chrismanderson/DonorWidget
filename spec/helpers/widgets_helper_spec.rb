bf93618e5f502c254daf343727984036b8444ba3
Mark Tabler <mark.tabler@fallingmanstudios.net>
2012-07-24 18:48:54 -0400

    Remove extraneous line

--- spec/helpers/widgets_helper_spec.rb
+++ spec/helpers/widgets_helper_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe WidgetsHelper do
   describe "#widget_id_meta" do
     it 'returns a meta tag for a given id' do
-      include WidgetsHelper
       widget = Widget.create(url: "sample_url")
       widget.update_attribute(:id, 1337)
       widget_id_meta(widget).should == "<meta content=\"1337\" name=\"widget_id\" />"
